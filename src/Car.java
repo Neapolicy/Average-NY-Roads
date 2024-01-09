915aa97caf92c38a23d672375b725835a54a18e2
Neapolicy <matthewlin559@gmail.com>
2024-01-09 10:13:23 -0500

    NEW CAR BABYYYYY

--- src/Car.java
+++ src/Car.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class Car extends JLabel implements Runnable{
     private Box carHitbox;
-    private String direction;
+    private String direction = "right";
     public Car(int x, int y) throws IOException {
         ImageIcon icon = new ImageIcon(ImageIO.read(new File("ImageFiles/Images/car1.png")));
         Image image = icon.getImage();
@@ -18,45 +18,17 @@ public class Car extends JLabel implements Runnable{
         this.setBounds(x, y, icon.getIconWidth(), icon.getIconHeight());
         this.setOpaque(true);
         carHitbox = (new Box(this.getX(), this.getY(), this.getWidth(), this.getHeight(), Color.RED));
-        pickDirection();
         Thread thread = new Thread(this);
         thread.start();
-    }
-
-    public void pickDirection()
-    {
-        Random rand = new Random();
-        int i = rand.nextInt(1, 5);
-        switch (i) {
-            case 1 -> direction = "up";
-            case 2 -> direction = "down";
-            case 3 -> direction = "left";
-            case 4 -> direction = "right";
-        }
-        System.out.println(direction);
     }
 
     private void moveCar()
     {
         int step = 30;
 
-        switch (direction) {
-            case "up" -> {
-                this.setLocation(this.getX(), this.getY() - step);
-                carHitbox.setLocation(this.getX(), this.getY());
-            }
-            case "left" -> {
-                this.setLocation(this.getX() - step, this.getY());
-                carHitbox.setLocation(this.getX(), this.getY());
-            }
-            case "down" -> {
-                this.setLocation(this.getX(), this.getY() + step);
-                carHitbox.setLocation(this.getX(), this.getY());
-            }
-            case "right" -> {
-                this.setLocation(this.getX() + step, this.getY());
-                carHitbox.setLocation(this.getX(), this.getY());
-            }
+        if (direction.equals("right")) {
+            this.setLocation(this.getX() - step, this.getY());
+            carHitbox.setLocation(this.getX(), this.getY());
         }
     }
 

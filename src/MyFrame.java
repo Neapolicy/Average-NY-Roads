07550714af1cda7d3c362ee8849b5043135c0462
neapolicy <matthewlin559@gmail.com>
2024-02-27 15:35:04 -0500

    asd

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -249,15 +249,6 @@ public class MyFrame extends JFrame implements Runnable { //make this in charge 
                 checkTrainPosition();
 //                add(road);
                 add(railroad);
-                Runtime rt = Runtime.getRuntime();
-
-                long total_mem = rt.totalMemory();
-
-                long free_mem = rt.freeMemory();
-
-                long used_mem = total_mem - free_mem;
-
-                System.out.println("Amount of used memory: " + used_mem);
             } catch (IOException | InterruptedException e) {
                 throw new RuntimeException(e);
             }

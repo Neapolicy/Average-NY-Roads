94227db4bbcafa19bfd3e2041ce91dda4994b7bb
neapolicy <matthewlin559@gmail.com>
2024-03-01 15:34:28 -0500

    made road

--- src/Road.java
+++ src/Road.java
@@ -2,21 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Road extends JPanel{
-    private int width;
     public Road(int width) {
         this.setLayout(null);
-        this.width = width;
     }
     @Override
     public void paintComponent(Graphics g) { //rahh
         super.paintComponent(g);
 
         // Paint your images
-        ImageIcon icon2 = new ImageIcon("ImageFiles/Images/background.jpg");
-        Image image2 = icon2.getImage();
-        Image newimg = image2.getScaledInstance(width, 300, Image.SCALE_SMOOTH);
-        icon2 = new ImageIcon(newimg);
-        g.drawImage(icon2.getImage(), 0, 0, this);
+        g.setColor(new Color(102, 102, 102)); //dark grey
+        g.fillRect(0, 0, (int) MyFrame.size.getWidth(), (int) MyFrame.size.getHeight() - 700);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, (int) MyFrame.size.getWidth(), (int) 30);
+        g.fillRect(0, this.getHeight() - 30, (int) MyFrame.size.getWidth(), (int) 30);
+
+        g.setColor(new Color(255, 204, 51)); //dark grey
+        for (int x = 10; x < this.getWidth(); x += 100){
+            g.fillRect(x, this.getHeight() / 2 - 10, 50, 20);
+        }
+
     }
 
     public void setSize(int width, int height) {

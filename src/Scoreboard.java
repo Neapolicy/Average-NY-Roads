3d19357dd3928fc9b6a9050929134fbd18105e5e
Neapolicy <matthewlin559@gmail.com>
2024-03-02 20:23:19 -0500

    fixed a few issues with cars tp'ing in

---
+++ src/Scoreboard.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Scoreboard extends JLabel {
+    public Scoreboard(String text){
+        Font font = new Font("Arial", Font.PLAIN, 30);
+        this.setFont(font);
+        this.setText(text);
+    }
+
+    public void updateScore(int score){
+        this.setText("Current Score: " + score);
+    }
+
+    public void displayCombo(int combo){
+        this.setText("Current Combo: " + combo);
+    }
+    public void displayTime(int time){
+        this.setText("Current Time: " + time);
+    }
+    public void potholesFilled(int filled){
+        this.setText("Potholes Filled: " + filled);
+    }
+}

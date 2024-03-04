09c5adb0600b4e6b1bd2839f75be108ec18a6746
neapolicy <matthewlin559@gmail.com>
2024-03-04 16:25:33 -0500

    optimized

--- src/Box.java
+++ src/Box.java
@@ -1,12 +1,7 @@
 import java.awt.*;
 
 public class Box extends Rectangle {
-    private Color color;
-
     public Box(int x, int y, int width, int height)
-    {
-        super(x, y, width, height);
-        this.color = new Color(0, 0, 0, 0);
-    }
+   {super(x, y, width, height);}
 
 }

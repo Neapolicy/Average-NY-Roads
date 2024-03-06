45526398a8936aceb141f40de972338a993dad98
Neapolicy <matthewlin559@gmail.com>
2024-03-06 18:08:07 -0500

    asd

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main
 {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         Mainframe main = new Mainframe();
         main.start();
     }

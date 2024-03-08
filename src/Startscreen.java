b8f1a6ef3847dd2a127905c150457d459258d383
Neapolicy <matthewlin559@gmail.com>
2024-03-08 17:08:03 -0500

    now displays text woohoo, now to make it look pretty

--- src/Startscreen.java
+++ src/Startscreen.java
@@ -3,10 +3,12 @@ public class Startscreen extends Mainframe {
         Scoreboard startText = new Scoreboard("Press E to Deploy a Bomb", (int) (MyFrame.size.getWidth() / 2) - 150, (int) (MyFrame.size.getHeight() / 2), 1000, 100);
         Scoreboard midText = new Scoreboard("Press Space to Fill In Potholes", (int) (MyFrame.size.getWidth() / 2) - 170, (int) (MyFrame.size.getHeight() / 2) - 100, 1000, 100);
         Scoreboard endText = new Scoreboard("Most Importantly, Don't Let The Cars Run Over You Or The Potholes", (int) (MyFrame.size.getWidth() / 2) - 400, (int) (MyFrame.size.getHeight() / 2) + 100, 1000, 100);
+        Scoreboard clickInfo = new Scoreboard("Click Anywhere to Begin", (int) (MyFrame.size.getWidth() / 2) - 400, (int) (MyFrame.size.getHeight() / 2) + 200, 1000, 100);
 
         frame.add(startText);
         frame.add(midText);
         frame.add(endText);
+        frame.add(clickInfo);
 
         refresh(); //guarantees that the text for appear, because the start text aint guaranteed for reasons god knows why
     }

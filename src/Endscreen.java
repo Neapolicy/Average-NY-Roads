4855cb0354cd9d95e6c620cdc762e4e6bc005745
Neapolicy <matthewlin559@gmail.com>
2024-03-07 22:29:36 -0500

    endscreen not displaying properly

--- src/Endscreen.java
+++ src/Endscreen.java
@@ -1,5 +1,9 @@
 public class Endscreen extends Mainframe{
-    public void loseScreen() {
-        getContentPane().removeAll();
+    public Endscreen() {
+        Scoreboard startText = new Scoreboard("Game Over!", (int) (MyFrame.size.getWidth() / 2), (int) (MyFrame.size.getHeight() / 2) - 100, 1000, 100);
+
+        frame.add(startText);
+
+        startText.setBounds((int) (MyFrame.size.getWidth() / 2), (int) (MyFrame.size.getHeight() / 2) - 100, 1000, 100);
     }
 }
--- src/Startscreen.java
+++ src/Startscreen.java
@@ -1,16 +1,9 @@
-import javax.swing.*;
-import java.io.IOException;
-
 public class Startscreen extends Mainframe {
     public Startscreen(){
-        startScreen();
-    }
-    public void startScreen() {
         Scoreboard startText = new Scoreboard("Press E to deploy a bomb", (int) (MyFrame.size.getWidth() / 2), (int) (MyFrame.size.getHeight() / 2) - 100, 1000, 100);
 
         frame.add(startText);
 
         startText.setBounds((int) (MyFrame.size.getWidth() / 2), (int) (MyFrame.size.getHeight() / 2) - 100, 1000, 100);
     }
-
 }

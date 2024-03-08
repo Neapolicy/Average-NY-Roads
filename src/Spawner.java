0b70606ec4d8a35b25d2bf4ec1a6ba4c2e8dba27
Neapolicy <matthewlin559@gmail.com>
2024-03-07 23:07:05 -0500

    split spawning function

---
+++ src/Spawner.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.util.Random;
+
+public class Spawner implements Runnable{
+    private final Random rand = new Random();
+    public Spawner(){
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+    public void addCars() throws IOException, InterruptedException //creates new cars
+    {
+        if (MyFrame.s.getTimePassed() % (MyFrame.countDowns[1] - (MyFrame.s.getTimePassed() / 15)) == 0 && (MyFrame.s.getTimePassed() != 0 && MyFrame.s.getTimePassed() != Car.timeLastSpawned)) //5 seconds to add a car is purely for testing purposes, also rewrite this without using sleep
+        {
+            int timesGenerated;
+            Car.timeLastSpawned = MyFrame.s.getTimePassed();
+            int lastSpawnedYCoord = 0;
+            if (MyFrame.countDowns[1] < 6) MyFrame.countDowns[1] = 6; //spawn timer min
+            timesGenerated = rand.nextInt(1, 5);
+            for (int i = 0; i < timesGenerated; i++) {
+                int y_axis = rand.nextInt(MyFrame.car_locations.length);
+                while (y_axis == lastSpawnedYCoord) y_axis = rand.nextInt(MyFrame.car_locations.length);
+                MyFrame.cars.add(new Car((int) MyFrame.size.getWidth() + 500, MyFrame.car_locations[y_axis]));
+                lastSpawnedYCoord = MyFrame.car_locations[y_axis];
+                Thread.sleep(300);
+            }
+            Car.speed += 2.5;
+        }
+    }
+    public void addPotholes() throws IOException {
+        if (MyFrame.s.getTimePassed() % (MyFrame.countDowns[0] - MyFrame.s.getTimePassed() / 10) == 0 && MyFrame.s.getTimePassed() != 0 && Pothole.timeLastSpawned != MyFrame.s.getTimePassed()) //5 seconds to add a car is purely for testing purposes
+        {
+            int timesGenerated;
+            if (MyFrame.countDowns[0] < 3) MyFrame.countDowns[0] = 3;
+            timesGenerated = rand.nextInt(1, 4);
+            int y_axis = rand.nextInt(300, 600);
+            int x_axis = rand.nextInt(100, 600);
+            for (int i = 0; i < timesGenerated; i++) {
+                MyFrame.potholes.add(new Pothole(x_axis, y_axis));
+            }
+            Pothole.timeLastSpawned = MyFrame.s.getTimePassed();
+        }
+    }
+    public void trainSummon() throws IOException {
+        if (MyFrame.s.getTimePassed() % 8 == 0 && MyFrame.s.getTimePassed() != 0 && MyFrame.s.getTimePassed() != Train.timeLastSpawned) {
+            new Train((int) MyFrame.size.getWidth(), 100);
+            Train.timeLastSpawned = MyFrame.s.getTimePassed();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (Gamestate.state != Gamestate.gameEnd) {
+            long startTime = System.nanoTime();
+            //do stuff per frame below
+            try {
+                trainSummon();
+                addCars();
+                addPotholes();
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            long totalTime = System.nanoTime() - startTime;
+
+            if (totalTime < 1000000000) {
+                try {
+                    Thread.sleep((1000000000 - totalTime) / 1000000); //theoretically should only be running at 1 fps
+                } catch (InterruptedException e) {
+                    System.out.close();
+                }
+            }
+        }
+    }
+}

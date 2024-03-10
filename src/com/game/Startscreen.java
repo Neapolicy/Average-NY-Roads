a8fceed66dc5abcd85e83f23c2dd3c33384230f2
Neapolicy <matthewlin559@gmail.com>
2024-03-09 19:15:00 -0500

    made it look prettier

--- src/com/game/Startscreen.java
+++ src/com/game/Startscreen.java
@@ -5,7 +5,7 @@ public class Startscreen extends Mainframe {
         int centerY = (int) (MyFrame.size.getHeight() / 2);
 
         Scoreboard titleCard = new Scoreboard("NY Simulator", centerX - 150, centerY - 300, 300, 100);
-        Scoreboard startText = new Scoreboard("Press E to Deploy a com.game.Bomb", centerX - 500, centerY, 1000, 100);
+        Scoreboard startText = new Scoreboard("Press E to Deploy a Bomb", centerX - 500, centerY, 1000, 100);
         Scoreboard midText = new Scoreboard("Press Space to Fill In The Potholes", centerX - 500, centerY - 100, 1000, 100);
         Scoreboard endText = new Scoreboard("Most Importantly, Don't Let The Cars Run Over You Or The Potholes", centerX - 500, centerY + 100, 1000, 100);
         Scoreboard clickInfo = new Scoreboard("Click Anywhere to Begin", centerX - 500, centerY + 200, 1000, 100);

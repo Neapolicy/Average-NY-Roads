6b5077dfa03faac0740a53f717a4cf08260a24d4
Neapolicy <matthewlin559@gmail.com>
2024-03-09 19:17:13 -0500

    made it look prettier/ text not as obnoxiously long

--- src/com/game/Endscreen.java
+++ src/com/game/Endscreen.java
@@ -3,11 +3,13 @@ package com.game;
 public class Endscreen extends Mainframe {
     private Sound sound = new Sound();
     public Endscreen(int score, int combo, int time) {
+        int centerX = (int) (MyFrame.size.getWidth() / 2);
+        int centerY = (int) (MyFrame.size.getHeight() / 2);
         sound.play("explosion", false);
-        Scoreboard endText = new Scoreboard("Game Over!", (int) (MyFrame.size.getWidth() / 2) - 50, (int) (MyFrame.size.getHeight() / 2) - 200, 1000, 100);
-        Scoreboard scoreText = new Scoreboard("Total Score: " + score, (int) (MyFrame.size.getWidth() / 2) - 100, (int) (MyFrame.size.getHeight() / 2) - 100, 1000, 100);
-        Scoreboard comboText = new Scoreboard("Highest Combo: " + combo, (int) (MyFrame.size.getWidth() / 2) - 100, (int) (MyFrame.size.getHeight() / 2), 1000, 100);
-        Scoreboard timeText = new Scoreboard("Time Elapsed: " + time, (int) (MyFrame.size.getWidth() / 2) - 100, (int) (MyFrame.size.getHeight() / 2) + 100, 1000, 100);
+        Scoreboard endText = new Scoreboard("Game Over!", centerX - 50, centerY - 200, 1000, 100);
+        Scoreboard scoreText = new Scoreboard("Total Score: " + score, centerX - 100, centerY - 100, 1000, 100);
+        Scoreboard comboText = new Scoreboard("Highest Combo: " + combo, centerX - 100, centerY, 1000, 100);
+        Scoreboard timeText = new Scoreboard("Time Elapsed: " + time, centerX - 100, centerY + 100, 1000, 100);
 
         Mainframe.frame.add(endText);
         Mainframe.frame.add(scoreText);

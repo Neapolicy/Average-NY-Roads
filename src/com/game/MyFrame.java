c9c92b57ec9183b21c9cc8f251e1aba0ce1e9229
Neapolicy <matthewlin559@gmail.com>
2024-03-10 14:34:57 -0400

    changed spawns

--- src/com/game/MyFrame.java
+++ src/com/game/MyFrame.java
@@ -76,6 +76,7 @@ public class MyFrame extends Mainframe implements Runnable { //make this in char
         for (Car car : cars) {
             if (player.getPlayerHitbox().intersects(car.getCarHitbox())) {
                 Gamestate.state = Gamestate.gameEnd;
+                frame.removeKeyListener(player);
                 setValues(player.getScore(), player.getCombo(), s.getTimePassed());
                 sound.play("car_screech", false);
                 checkGameState();
@@ -84,6 +85,7 @@ public class MyFrame extends Mainframe implements Runnable { //make this in char
             for (Pothole pothole : potholes) {
                 if (car.getCarHitbox().intersects(pothole.getPotholeHitbox())) {
                     Gamestate.state = Gamestate.gameEnd;
+                    frame.removeKeyListener(player);
                     setValues(player.getScore(), player.getCombo(), s.getTimePassed());
                     sound.play("car_screech", false);
                     checkGameState();

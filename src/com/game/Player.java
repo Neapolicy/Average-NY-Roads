99db4602077d3493b2b77e1542d8b94a2a2d578c
neapolicy <matthewlin559@gmail.com>
2024-03-11 15:34:44 -0400

    asd

--- src/com/game/Player.java
+++ src/com/game/Player.java
@@ -19,7 +19,7 @@ public class Player extends JLabel implements KeyListener {
     private boolean down;
     private boolean left;
     private boolean right;
-    private int step = 15;
+    private int step = 10;
     private com.game.Box playerHitbox; //https://stackoverflow.com/questions/40252221/java-how-to-use-an-object-from-one-mouselistener-to-another-class cheque it out idk
 
     public Player() throws IOException {
